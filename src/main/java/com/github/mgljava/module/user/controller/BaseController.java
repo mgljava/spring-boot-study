6d610545e1df844cddb63a59051df28d0944f8ca
SweepMonk <guangliang1120@gmail.com>
2018-09-17 11:20:09 +0800

    [SpringBoot-2][Monk] test

--- src/main/java/com/github/mgljava/module/user/controller/BaseController.java
+++ src/main/java/com/github/mgljava/module/user/controller/BaseController.java
@@ -1,8 +1,12 @@
 package com.github.mgljava.module.user.controller;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class BaseController {
 
   protected BaseController() {
+    log.info("start BaseController!");
   }
 
   protected static final String SUCCESS = "success";
